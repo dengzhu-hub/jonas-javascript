19ac9c03b7f181961647bac41d9281e2ca04008d
dengzhu-hub <3052113768@qq.com>
2023-04-12 19:36:33 +0800

    token will update

--- OOP/script.js
+++ OOP/script.js
@@ -530,29 +530,28 @@ jay.clacAge();
 */
 
 class Account {
-    constructor(owner, currency, pin) {
-        this.owner = owner;
-        this.currency = currency;
-        this.pin = pin;
-        this.locale = navigator.language;
-        this.movements = []
-    };
-    deposit(val) {
-        this.movements.push(val);
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.locale = navigator.language;
+    this.movements = [];
+  }
+  deposit(val) {
+    this.movements.push(val);
+  }
+  withdraw(val) {
+    this.deposit(-val);
+  }
+  approveLoan(val) {
+    return true;
+  }
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log('Load approved');
     }
-    withdraw(val) {
-        this.deposit(-val);
-    }
-    approveLoan(val) {
-        return true;
-    }
-    requestLoan(val) {
-        if (this.approveLoan(val)) {
-            this.deposit(val);
-            console.log('Load approved');
-            
-        }
-    }
+  }
 }
 
 const acc1 = new Account('Jonas', 'en-zhu', '1111');
@@ -562,3 +561,5 @@ acc1.withdraw(450);
 console.log(acc1);
 acc1.requestLoan(1000);
 acc1.approveLoan(1000);
+console.log(acc1.pin);
+console.log(acc1.owner);
